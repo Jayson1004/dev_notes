6abf7e002b08ffe6030e45217037a200b68d940e
jinxin <jinxin@111.com.cn>
2019-01-17 17:50:05 +0800

    m

---
+++ precache-manifest.05fc420f5cb305c4c946af4d1bd4e46a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "c4aac0bac9b4ced1cce7",
+    "url": "/js/chunk-vendors.5e90cb32.js"
+  },
+  {
+    "revision": "924334b569c987079ef8",
+    "url": "/js/app.c034a15c.js"
+  },
+  {
+    "revision": "260d945a8ff53ba43730",
+    "url": "/js/about.ecbc5746.js"
+  },
+  {
+    "revision": "710a267fbbef6623e1d9699613c15bae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "924334b569c987079ef8",
+    "url": "/css/app.b618cb66.css"
+  }
+];
