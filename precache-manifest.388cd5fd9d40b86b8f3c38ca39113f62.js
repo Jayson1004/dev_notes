d52f950fb1a2c04b79be21983a3e22814bcb1810
jinxin <jinxin@111.com.cn>
2020-02-18 20:41:48 +0800

    m

---
+++ precache-manifest.388cd5fd9d40b86b8f3c38ca39113f62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e302d5f331bd245cadfffd9ef749251",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f8d2d6d200b9ab44a58",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8af1c97d36f0b1426ae8",
+    "url": "/static/js/2.b6fabe4d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.b6fabe4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f8d2d6d200b9ab44a58",
+    "url": "/static/js/main.f0921af0.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
